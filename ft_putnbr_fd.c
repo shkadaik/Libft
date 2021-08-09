e7724001e771a966b528552e16b9cf1a988beb46
shkadaik <kadaikar42@gmail.com>
2021-08-09 15:55:22 +0200

    correct

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -19,7 +19,7 @@ void	ft_putchar_fd(char c, int fd)
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n >= 0 && nb <= 9)
+	if (n >= 0 && n <= 9)
 		ft_putchar_fd(n + '0', fd);
 	else if (n == -214783648)
 	{
